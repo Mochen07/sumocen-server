e23b456463f950fb2741aa9babee8cd0f22d485e
Mochen07 <mochen7777@gmail.com>
2020-03-01 17:01:12 +0800

    add right control

--- src/middle_wares/login_pass.js
+++ src/middle_wares/login_pass.js
@@ -1,3 +1,4 @@
+// 权限控制暂时有问题 只要authorization有值就会通过验证
 import {ResultJsonFormat} from './../config/global-func'
 
 export default (req, res, next) => {
@@ -5,8 +6,8 @@ export default (req, res, next) => {
     if (req.path.indexOf('/back') === -1) return next()
 
     // 2. 判断所有的后端请求是否处于已经登录状态
-    console.log(req.session, '查看权限控制保存信息')
-    if(req.session.token) return next()
+    console.log(req.headers.authorization, '查看权限控制保存信息')
+    if(req.headers.authorization) return next()
 
     // 3. 没有登录
     if(req.path.indexOf('/back/api/') !== -1){
