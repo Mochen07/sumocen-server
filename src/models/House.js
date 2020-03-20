ceed7fb5da6302db34d7a7a7dcbe24ca6ffb02c2
Mochen07 <mochen7777@gmail.com>
2020-03-20 18:27:33 +0800

    add house schema

---
+++ src/models/House.js
@@ -0,0 +1,17 @@
+// House
+const houseSchema = mongoose.Schema({
+    pictureList: {type: Array, required: true}, // 照片组
+    province: {type: String, default: '未定义'}, // 详细地址
+    address: {type: String, default: '未定义'}, // 省
+    city: {type: String, default: '未定义'}, // 市
+    area: {type: String, required: false}, // 区
+    plot: {type: String, default: '未定义'}, // 小区
+    price: {type: Number, required: false}, // 价格
+    grade: {type: Number, required: false}, // 评分
+    joinDate: {type: Date, default: Date.now()}, // 添加时间
+    lastEditDate: {type: Date, default: Date.now()}, // 添加时间
+})
+
+const House = mongoose.model('house', houseSchema)
+
+export default House
