ac81d5e0dd32701b616a0fe9f8090363e3c25b70
mochen <mochen7777@gmail.com>
2022-12-06 16:29:49 +0800

    新增注册用户接口。

--- services/user.js
+++ services/user.js
@@ -2,9 +2,20 @@ const User = require('../models/index').getModel('user')
 
 const user = {
     async login (userData) {
-        let result = await User.findOne(userData)
-        console.log(result)
-        return result
+      let result = await User.findOne(userData)
+      return result
+    },
+    // 注册一个用户
+    async add (userData) {
+      let isFindOne = await User.findOne(userData)
+      if (isFindOne) {
+        return false
+      }
+      let user = new User()
+      user.userName = userData.userName
+      user.password = userData.password
+      let result = await user.save()
+      return result
     }
 }
 
