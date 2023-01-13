3750449fa21cc5ec7ccb7186d615f4e93ed6d6ff
mochen <mochen7777@gmail.com>
2023-01-14 00:22:26 +0800

    修改登录异常提示语不正确

--- controllers/user.js
+++ controllers/user.js
@@ -8,12 +8,13 @@ const login = {}
 
 // 注册
 login.register = async (ctx, next) => {
-  const {username, password} = ctx.request.body
+  const { username, password } = ctx.request.body
   if (!username || !password) {
     throw new InvalidQueryError()
   }
   const user = await userServices.add({
-    username, password,
+    username,
+    password,
   })
   // result 1 用户已注册
   if (!user) {
@@ -27,26 +28,30 @@ login.register = async (ctx, next) => {
 
 // 登录
 login.login = async (ctx, next) => {
-    const {username, password} = ctx.request.body
-    if (!username || !password) {
-        throw new InvalidQueryError()
-    }
-    const user = await userServices.login({
-        username: username,
-        password: password
-    })
-    if (!user) {
-        ctx.result = ''
-        ctx.msg = '用户名或密码不正确'
-    } else {
-        ctx.result = jwt.sign({
-            data: user._id,
-            // 设置 token 过期时间 60 seconds * 60 minutes = 1 hour
-            exp: Math.floor(Date.now() / 1000) + (60 * 60 * 24),
-        }, config.secret)
-        ctx.msg = '登录成功'
-    }
-    return next()
+  const { username, password } = ctx.request.body
+  if (!username || !password) {
+    throw new InvalidQueryError()
+  }
+  const user = await userServices.login({
+    username: username,
+    password: password,
+  })
+  if (!user) {
+    ctx.code = -1
+    ctx.result = ''
+    ctx.msg = '用户名或密码不正确'
+  } else {
+    ctx.result = jwt.sign(
+      {
+        data: user._id,
+        // 设置 token 过期时间 60 seconds * 60 minutes = 1 hour
+        exp: Math.floor(Date.now() / 1000) + 60 * 60 * 24,
+      },
+      config.secret
+    )
+    ctx.msg = '登录成功'
+  }
+  return next()
 }
 
 module.exports = login
